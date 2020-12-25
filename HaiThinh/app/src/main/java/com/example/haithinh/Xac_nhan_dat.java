a56b342b5acfebd60391de107cea88fccb81dbcc
hainguyen99-cdm <haidb.1280@gmail.com>
2020-12-25 13:06:03 +0100

    dang nhap dang xuat

---
+++ HaiThinh/app/src/main/java/com/example/haithinh/Xac_nhan_dat.java
@@ -0,0 +1,14 @@
+package com.example.haithinh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Xac_nhan_dat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xac_nhan_dat);
+    }
+}
